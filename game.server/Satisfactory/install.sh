55025689ae5ba1843b0f16e4d3e6abcde36e72fa
Theo Reichert <theo.reichert7@gmail.com>
2022-05-31 22:04:09 +0200

    fixed non existing folder

--- game.server/Satisfactory/install.sh
+++ game.server/Satisfactory/install.sh
@@ -3,7 +3,9 @@
 steamcmd +force_install_dir "~/SatisfactoryDedicatedServer" +login 'anonymous' +app_update '1690800' 'validate' +quit
 
 sudo cp "./SatisfactoryDS.service" '/etc/systemd/system/'
-sudo cp "./start.sh" "~/SatisfactoryDedicatedServer/"
+
+mkdir "~/SatisfactoryDedicatedServer" && \
+sudo cp "./start.sh" "~/SatisfactoryDedicatedServer/" && \
 sudo chmod +x "~/SatisfactoryDedicatedServer/start.sh"
 
 sudo touch '/var/log/satisfactory.log'
