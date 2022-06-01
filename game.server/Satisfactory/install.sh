f188a2367ec6e0f8ff4b3b4cec7392926597b8fc
Theo Reichert <theo.reichert7@gmail.com>
2022-06-01 12:11:42 +0200

    reorder

--- game.server/Satisfactory/install.sh
+++ game.server/Satisfactory/install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-steamcmd +force_install_dir "~/SatisfactoryDedicatedServer" +login 'anonymous' +app_update '1690800' 'validate' +quit
-
-sudo cp "./SatisfactoryDS.service" '/etc/systemd/system/'
-
+sudo cp "./SatisfactoryDS.service" '/etc/systemd/system/' && \
 mkdir "~/SatisfactoryDedicatedServer" && \
 sudo cp "./start.sh" "~/SatisfactoryDedicatedServer/" && \
-sudo chmod +x "~/SatisfactoryDedicatedServer/start.sh"
-
-sudo touch '/var/log/satisfactory.log'
-sudo touch '/var/log/satisfactory.err'
+sudo chmod +x "~/SatisfactoryDedicatedServer/start.sh" && \
+sudo touch '/var/log/satisfactory.log' && \
+sudo touch '/var/log/satisfactory.err' && \
+steamcmd +force_install_dir "~/SatisfactoryDedicatedServer" +login 'anonymous' +app_update '1690800' 'validate' +quit
