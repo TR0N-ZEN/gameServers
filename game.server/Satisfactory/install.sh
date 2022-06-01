e40a277199ffed7a6db83c00edb44f58d2095716
Theo Reichert <theo.reichert7@gmail.com>
2022-06-01 13:42:06 +0200

    removed unecessary lines

--- game.server/Satisfactory/install.sh
+++ game.server/Satisfactory/install.sh
@@ -5,7 +5,5 @@ INSTALL_TARGET=~/SatisfactoryDedicatedServer
 
 sudo cp "$PREFIX/SatisfactoryDS.service" '/etc/systemd/system/' && \
 mkdir "$INSTALL_TARGET" && \
-# sudo cp ~/$PREFIX/start.sh  ~/SatisfactoryDedicatedServer/ && \
-# sudo chmod +x "$INSTALL_TARGET/start.sh" && \
 sudo touch '/var/log/satisfactory.log' '/var/log/satisfactory.err' && \
 steamcmd +force_install_dir $INSTALL_TARGET +login 'anonymous' +app_update '1690800' 'validate' +quit
