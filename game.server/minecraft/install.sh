d713cd7c8964a683f57247976689700a352622f5
Theo Reichert <theo.reichert7@gmail.com>
2022-06-02 16:11:06 +0200

    not yet tested

--- game.server/minecraft/install.sh
+++ game.server/minecraft/install.sh
@@ -1,15 +1,16 @@
-suo useradd -m -r -d /opt/minecraft minecraft # why these options m,r and d
-cd '/home/minecraft'
+sudo useradd -m -r -d /opt/minecraft minecraft # why these options m,r and d?
+cd '/opt/minecraft'
 # sudo apt install 'default-jdk'
 # sudo apt install 'openjdk-11-headless'
-sudo apt install 'openjdk-17-headless'
-# sudo ufw allow 25565/tcp # firewall port opening
+sudo apt install 'openjdk-17-headless' # install java development kit, might need another version as in lines above
+# sudo ufw allow 25565/tcp # opening a port in the firewall via the program ufw
 
-curl 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar' -o 'minecraft_server.jar'
-java -Xmx4G -Xms4G -jar ./minecraft_server.jar nogui
-sed -n -i 'eula.txt' -e 's/eula=false/eula=true/gp'
+curl 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar' -o 'minecraft_server.jar' # vanialla aminecraft
+java -Xmx4G -Xms4G -jar ./minecraft_server.jar nogui # first execution will fail
+sed -n -i 'eula.txt' -e 's/eula=false/eula=true/gp' # change false to true otherwise the server will crash
 
-cp 'MinecraftDS.service' 'etc/systemd/system'
-systemctl daemon-reload
-systemctl start 'MinecraftDS.service'
+cp 'MinecraftDS.service' 'etc/systemd/system' # copy systemd .service file to one of systemd's predefined folders
+systemctl daemon-reload # reload systemd daemon to recognize the new .service file
+systemctl start 'MinecraftDS.service' # start the server via systemd and the MinecraftDS.service file
 
+# further configuration of the server via command line arguments or the server.properties file, here inside /opt/minecraft
