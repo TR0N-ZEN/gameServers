a06fb7dd0d887378f05274a676fb7b1b2058a55f
Theo Reichert <theo.reichert7@gmail.com>
2022-06-24 13:41:39 +0200

    absolute paths

--- game.server/minecraft/install.sh
+++ game.server/minecraft/install.sh
@@ -5,9 +5,9 @@ cd '/opt/minecraft'
 sudo apt install 'openjdk-17-headless' # install java development kit, might need another version as in lines above
 # sudo ufw allow 25565/tcp # opening a port in the firewall via the program ufw
 
-curl 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar' -o 'minecraft_server.jar' # vanialla aminecraft
-java -Xmx4G -Xms4G -jar ./minecraft_server.jar nogui # first execution will fail
-sed -n -i 'eula.txt' -e 's/eula=false/eula=true/gp' # change false to true otherwise the server will crash
+curl 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar' -o 'server.jar' # vanialla aminecraft
+java -Xmx4G -Xms4G -jar /opt/minecraft/server.jar nogui # first execution will fail
+sed -n -i '/opt/minecraft/eula.txt' -e 's/eula=false/eula=true/gp' # change false to true otherwise the server will crash
 
 cp 'MinecraftDS.service' 'etc/systemd/system' # copy systemd .service file to one of systemd's predefined folders
 systemctl daemon-reload # reload systemd daemon to recognize the new .service file
