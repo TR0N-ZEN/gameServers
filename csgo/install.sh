52a9533c477eb8af4feafe77439056e03c2e3a1c
Theo Reichert <theo.reichert7@gmail.com>
2022-07-29 20:06:27 +0200

    style

--- csgo/install.sh
+++ csgo/install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # if errors uccur involving uninstalled packages like the following uncomment the following lines
 #sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 libncurses5 lib32z1
-mkdir /home/steam/csgo &&\
+
+sudo touch /var/log/csgo.{log,error} && \
+sudo chown steam:steam /var/log/csgo* && \
+
+mkdir /home/steam/csgo && \
 /home/steam/steamcmd/steamcmd.sh \
-  +force_install_dir /home/steam/csgo \
+  +force_install_dir '/home/steam/csgo' \
   +login 'anonymous' \
   +app_update '740' 'validate' \
-  +quit
-cp csgo/start.sh /home/steam/csgo/
-sudo chmod 500 /home/steam/csgo/start.sh
-sudo chown steam:steam /home/steam/csgo/start.sh
-echo 'Remember to replace <x> inside /home/steam/csgo/start.sh with a steam-gameserver-id :->'
-sudo cp csgo/server.service /etc/systemd/system/csgo.server.service
-sudo systemctl daemon-reload
+  +quit && \
+cp csgo/start.sh /home/steam/csgo/ && \
+sudo chown -R steam:steam /home/steam/csgo && \
+sudo chmod 500 /home/steam/csgo/start.sh && \
+
+sudo cp csgo/server.service /etc/systemd/system/csgo.server.service && \
+sudo systemctl daemon-reload && \
+
+echo 'Remember to replace <x> inside "/home/steam/csgo/start.sh" with a steam-gameserver-id :->'
--- satisfactory/install.sh
+++ satisfactory/install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #su steam && cd
+
+sudo touch /var/log/satisfactory.{log,error} && \
+sudo chown steam:steam /var/log/satisfactory* && \
+
 mkdir /home/steam/satisfactory && \
-sudo touch '/var/log/satisfactory.log' '/var/log/satisfactory.err' && \
-sudo chown steam:steam '/var/log/satisfactory.log' '/var/log/satisfactory.err' && \
 /home/steam/steamcmd/steamcmd.sh \
   +force_install_dir '/home/steam/satisfactory' \
   +login 'anonymous' \
   +app_update '1690800' 'validate' \
   +quit && \
-sudo ln '/home/steam/gameServers/satisfactory/server.service' '/etc/systemd/system/satisfactory.server.service' && \
+
+sudo chown -R steam:steam /home/steam && \
+
+
+sudo cp satisfactory/server.service /etc/systemd/system/satisfactory.server.service && \
 sudo systemctl daemon-reload
