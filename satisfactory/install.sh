fb50c1f91be7bf0c525016b51d0a1024187ab14c
Theo Reichert <theo.reichert7@gmail.com>
2024-06-26 00:34:00 +0000

    update satisfactory installation
    + change installation folder since
      https://docs.ficsit.app/satisfactory-modding/latest/Development/BeginnersGuide/project_setup.html
      states "This avoids long path issues"
    + remove comments from .service file
    + other minorities

--- satisfactory/install.sh
+++ satisfactory/install.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+sudo systemctl stop satisfactory.server.service
+
 sudo touch /var/log/satisfactory.{log,error} && \
 sudo chown steam:steam /var/log/satisfactory*
 
 sudo -u steam bash -i << EOF
+mkdir ~/SatisfactoryDedicatedServer && \
 steamcmd \
+  +force_install_dir ~/SatisfactoryDedicatedServer \
   +login 'anonymous' \
-  +app_update '1690800' 'validate' \
+  +app_update '1690800' \
+  -beta public 'validate' \
   +quit
 EOF
 
 sudo cp \
-	/home/game_server_installer/gameServers/satisfactory/server.service \
+	${PATH_TO_GIT_REPOSITORY}/satisfactory/server.service \
 	/etc/systemd/system/satisfactory.server.service 
 
 sudo systemctl daemon-reload
+sudo systemctl status satisfactory.server.service
+sudo systemctl start satisfactory.server.service
+sudo systemctl status satisfactory.server.service
 
+# to inspect logs use the following commands:
+#   sudo tail -f /var/log/satisfactory.log
+#   sudo tail -f /var/log/satisfactory.error
+#   sudo systemctl -u satisfactory.server.service -f
--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@
 # adduser game_server_installer
 # su game_server_installer
 
-cd ~
+export PATH_TO_GIT_REPOSITORY=~/gameServers
+
+cd /tmp && \
 curl 'https://github.com/charmbracelet/gum/releases/download/v0.1.0/gum_0.1.0_linux_amd64.deb' -OL &&\
 sudo dpkg -i 'gum_0.1.0_linux_amd64.deb' &&\
 rm 'gum_0.1.0_linux_amd64.deb' && \
-find . -name 'install.sh' | gum filter | xargs bash
+cd ~ && \
+find ${PATH_TO_GIT_REPOSITORY} -name 'install.sh' | gum filter | xargs bash
 
