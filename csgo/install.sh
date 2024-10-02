be1d0422c4d9864aebceeb21342b79a1bbf05663
Theo Reichert <38770238+TR0N-ZEN@users.noreply.github.com>
2024-10-02 20:59:17 +0200

    Updates dependencies of csgo

--- csgo/install.sh
+++ csgo/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # if errors uccur involving uninstalled packages like the following uncomment the following lines
-#sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 libncurses5 lib32z1
+#sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc-s1 libncurses5 lib32z1
 
 sudo touch /var/log/csgo.{log,error} && \
 sudo chown steam:steam /var/log/csgo*
